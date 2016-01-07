adab999ae5a55e3b328534aebfcf711867c4dd56
Sajin M <sajinrules@gmail.com>
2016-01-07 14:08:26 +0530

    drag and drop implimented

--- scripts/components/Day.js
+++ scripts/components/Day.js
@@ -7,6 +7,30 @@ var Day = React.createClass({
 	handleClick:function() {
 		this.refs.leftNav.refs.leftNav.toggle();
 	},
+	dragStart: function(e) {
+		//this.dragged = e.currentTarget;
+		e.dataTransfer.effectAllowed = 'move';
+		e.dataTransfer.setData("text", e.target.id);
+	},
+	dragEnd: function(e) {
+		
+	},
+	dragOver: function(e) {
+		e.preventDefault();
+	},
+	drop : function(e) {
+		console.log("e",e);
+		console.log("e.currentTarget",e.currentTarget.children[3].children[0]);
+		console.log("e.target:",e.target);
+		console.log("e.target.children:",e.target.children);
+		var data = e.dataTransfer.getData("text");
+		if(e.target.children.length > 0)
+			e.target.children[0].appendChild(document.getElementById(data));
+		else
+			e.currentTarget.children[3].children[0].appendChild(document.getElementById(data))
+		
+		e.preventDefault();
+	},
 	render: function() {
 		var count = this.props.count;
 		var data = this.props.data || {};
@@ -15,8 +39,8 @@ var Day = React.createClass({
 		var index = this.props.index;
 
 		var colStyle='';
-		var col= {float:'left',width: '13.6%',textAlign:'left',display: 'block',padding:'5px',borderBottom:'1px solid #ddd',borderLeft:'1px solid #ddd'};
-		var colLast= {float:'left',width: '13.6%',textAlign:'left',display: 'block',padding:'5px',borderBottom:'1px solid #ddd',borderLeft:'1px solid #ddd',borderRight:'1px solid #ddd'};
+		var col= {float:'left',width: '13.6%',textAlign:'left',display: 'block',padding:'5px',borderBottom:'1px solid #ddd',borderLeft:'1px solid #ddd',overflow:'hidden'};
+		var colLast= {float:'left',width: '13.6%',textAlign:'left',display: 'block',padding:'5px',borderBottom:'1px solid #ddd',borderLeft:'1px solid #ddd',borderRight:'1px solid #ddd',overflow:'hidden'};
 		var newIndex = index+1;
 		if(newIndex%7===0 || newIndex===count){
 			colStyle=colLast
@@ -29,12 +53,29 @@ var Day = React.createClass({
 			return current===day
 		});
 		var style ={float: 'right'}
+		var listItems = events.map(function(item, i) {
+			return (
+				<li data-id={i}
+					id={current+'-'+i}
+				    key={i}
+				    draggable="true"
+				    onDragEnd={this.dragEnd}
+				    onDragStart={this.dragStart}
+				    >
+				  {item.title}
+				</li>
+			);
+		},this);
 		return (
-			<div style={colStyle} className="day" onClick={this.handleClick}>{current}
+			<div style={colStyle} className="day" onClick={this.handleClick} onDrop={this.drop} onDragOver={this.dragOver}>{current}
 			{
 				(events.length > 0) ? <div style={style}><Badge badgeContent={events.length} secondary={true}></Badge></div> : ""
 			}
 				<Leftnav ref="leftNav" docked={false} data={events}></Leftnav>
+				<div  style={{overflow:'auto',height:'120px',width:'100%'}} >
+					<ul id="drop" style={{padding:'2px',margin:'0px '}}>{listItems}
+					</ul>
+				</div>
 			</div>
 		);
 	}
